22928fe10a4f543a64609f2b214aab412c3e5f81
Daniel Costa <costa.xdaniel@gmail.com>
2023-07-26 18:14:12 +0000

    reports

--- database/patch/revisions/critical/012-2023-06-v1.1.0-iva/0003-iva-serie.sql.ts
+++ database/patch/revisions/critical/012-2023-06-v1.1.0-iva/0003-iva-serie.sql.ts
@@ -1,6 +1,6 @@
 import {block} from "../../../core/updater";
 
-block( module, { identifier: "iva-structure-ncv2", flags:[ "@unique" ]} ).sql`
+block( module, { identifier: "iva-structure-ncv2", flags:[ "@unique"]} ).sql`
 do $$
 declare 
 begin
@@ -91,7 +91,6 @@ declare
   _autorizacao_continue tweeks.autorizacao;
   arg_colaborador_id uuid not null default args->>'arg_colaborador_id';
   arg_espaco_auth uuid not null default args->>'arg_espaco_auth';
-  _autorizacao_continue tweeks.autorizacao;
   arg_branch_uid uuid default tweeks.__branch_uid( arg_colaborador_id, arg_espaco_auth );
   _res_serie jsonb default jsonb_build_array();
   _next record;
