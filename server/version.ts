1a92c39d74435799a6fba2d9e13720148768a64d
Daniel Costa <costa.xdaniel@gmail.com>
2023-08-17 22:40:47 +0000

    reports

--- server/version.ts
+++ server/version.ts
@@ -2,19 +2,20 @@ import fs from "fs"
 import * as Path from "path";
 let incremented = false;
 //language=file-reference
-export let VERSION = fs.readFileSync( Path.join( __dirname, "../VERSION")).toString("utf-8")
-    .trim()
-    .split("\n")
-    .map( value => value.trim() )
-    .filter( value => value.length )
-    [ 0 ]
-;
+export let VERSION = {
+    NUMBER :fs.readFileSync( Path.join( __dirname, "../VERSION")).toString("utf-8")
+        .trim()
+        .split("\n")
+        .map( value => value.trim() )
+        .filter( value => value.length )
+        [ 0 ],
 
-export function incrementVersion(){
-    if( incremented ) return false;
-    let versionsParts = VERSION.split("." ).map( value => Number(value ) );
-    versionsParts[2]++;
-    VERSION = versionsParts.join(".");
-    fs.writeFileSync( Path.join( __dirname, "../VERSION"), VERSION );
-    return  VERSION;
-}
+    incrementor(){
+        if( incremented ) return false;
+        let versionsParts = VERSION.NUMBER.split("." ).map( value => Number(value ) );
+        versionsParts[2]++;
+        VERSION.NUMBER = versionsParts.join(".");
+        fs.writeFileSync( Path.join( __dirname, "../VERSION"), VERSION.NUMBER );
+        return  VERSION;
+    }
+};
