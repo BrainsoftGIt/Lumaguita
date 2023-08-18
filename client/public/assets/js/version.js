16b7733fa9ba0f19f23ae7d423c042b4de6327ef
ahmedjorge <ahmedferreira11@gmail.com>
2023-08-18 14:12:04 +0000

    commit laucher

--- client/public/assets/js/version.js
+++ client/public/assets/js/version.js
@@ -1,8 +1,8 @@
-let version = await new Promise((resolve) => {
+window.version = await new Promise((resolve) => {
     fetch(`${location.origin}/VERSION`).then(function (response) {
         return response.text();
     }).then(function (text) {
         resolve(text)
     });
 })
-window.document.getElementById("version").textContent = `v${version}`
+window.document.getElementById("version").textContent = `v${window.version}`;
