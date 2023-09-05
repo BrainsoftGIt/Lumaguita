bc418743272245f11b714c93d2a1a0a1e78f6575
Daniel Costa <danielcarvalho555@gmail.com>
2023-09-05 20:52:01 +0000

    dsdsds

---
+++ server/service/database.service/kitres/index.ts
@@ -0,0 +1,24 @@
+import {args} from "../../../global/args";
+import {PgCore} from "kitres";
+import {Client, ClientConfig, Pool} from "pg";
+
+
+
+
+let configs:ClientConfig = {
+    host: args.dbHost,
+    port: args.dbPort,
+    database: args.dbName,
+    user: args.dbUser,
+    password: args.dbPassword,
+}
+export const pgCore:PgCore = new PgCore( {
+    poolFactory:() => new Pool( configs ),
+    clientFactory:() => new Client( configs )
+});
+
+let replicateConfigs:ClientConfig = { ...configs, user: args.dbUserClone, password:  args.dbPasswordClone }
+export const replicatePgCore = new PgCore( {
+    poolFactory:() => new Pool( replicateConfigs ),
+    clientFactory: () => new Client( replicateConfigs)
+});
