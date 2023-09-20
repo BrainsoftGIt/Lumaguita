a1aa4cfab48466fa6d7774c2ea46a39c52a80ac3
Daniel Costa <danielcarvalho555@gmail.com>
2023-09-20 15:48:04 +0000

    dsdsds

--- database/patch/revisions/critical/014-2023-09-v2.0.6/0001-reports.sql.ts
+++ database/patch/revisions/critical/014-2023-09-v2.0.6/0001-reports.sql.ts
@@ -46,7 +46,8 @@ block( module, { identifier: "parametrized-reports-structure-force-02", flags:["
     <path d="M79.567,29.924l-18.26-18.479C61.025,11.16,60.641,11,60.24,11H20.5c-0.828,0-1.5,0.672-1.5,1.5v75
     c0,0.828,0.672,1.5,1.5,1.5h58c0.828,0,1.5-0.672,1.5-1.5V30.979C80,30.585,79.845,30.206,79.567,29.924z M62,16.415L74.929,29.5H62
     V16.415z M22,86V14h37v17c0,0.828,0.672,1.5,1.5,1.5H77V86H22z"/>
-    </svg>', 'Nota Credito', '../includes/notaCredito.html', 1, 0, 0, 0, 3)
+    </svg>', 'Nota Credito', '../includes/notaCredito.html', 1, 0, 0, 0, 3),
+    (116, 71, 'maguita.pos.report', '71', 1, null, 'Relatório', null,1,0,0,0,0)
     ) select lib.sets( jsonb_populate_record( null::auth.menu, to_jsonb( m ) ) )
     from ___menu m ;
 
