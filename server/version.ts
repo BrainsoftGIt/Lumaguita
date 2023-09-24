3db8e0db1addd7056e2ef11c536aee375242910b
Daniel Costa <danielcarvalho555@gmail.com>
2023-09-24 21:57:14 +0000

    v2.1.1

--- server/version.ts
+++ server/version.ts
@@ -27,7 +27,6 @@ export let VERSION = {
         versionsParts[2]++;
         VERSION.NUMBER = versionsParts.join(".");
         fs.writeFileSync( Path.join( __dirname, "../VERSION"), VERSION.NUMBER );
-        fs.writeFileSync( Path.join( __dirname, "../client/public/VERSION"), VERSION.NUMBER );
         return  VERSION;
     },
     get TAG(){
