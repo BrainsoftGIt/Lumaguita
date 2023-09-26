2e03380449bd49b289b1c8bdd79f54b0994cbf15
Daniel Costa <danielcarvalho555@gmail.com>
2023-09-26 03:55:12 +0000

    v2.1.1

---
+++ database/revs/revisions/zsample/sample.sql.ts
@@ -0,0 +1,77 @@
+import {patch, patchSQL, sql} from "kitres";
+
+export const mySampleRevision = sql`
+select 'myRevision1';
+`;
+
+export const myUniqueRevision = patch( { unique: true}, sql`
+  select 'myUniqueRevision'
+`);
+
+
+export const myUniqueRevision2 = patchSQL({unique: true}).sql`
+  select 'myUniqueRevision2';
+`;
+
+
+let myCondition = true;
+export const myConditionalRevision1 = patch( { when:myCondition}, sql`
+  select 'myConditionalRevision1';
+`);
+
+
+export const myConditionalRevision3 = patchSQL({
+    when: () => {
+        let myCondition = true;
+        return myCondition;
+    }
+}).sql`
+select 'myConditionalRevision3';
+`;
+
+export const myAsyncConditionalRevision3 = patchSQL({
+    when: () => {
+        return new Promise( resolve => {
+            setTimeout( ()=>{
+                resolve( true )
+            }, 5000 );
+        });
+    }
+}).sql`
+select 'myAsyncConditionalRevision';
+`;
+export const myAsyncConditionalRevision4 = patchSQL({
+    when: () => {
+        return new Promise( resolve => {
+            setTimeout( ()=>{
+                resolve( true )
+            }, 10000 );
+        });
+    }
+}).sql`
+select 'myAsyncConditionalRevision';
+`;
+
+export const myAsyncConditionalRevision = patchSQL({
+    when: () => {
+        return new Promise( resolve => {
+            setTimeout( ()=>{
+                resolve( true )
+            }, 20000 );
+        });
+    }
+}).sql`
+select 'myAsyncConditionalRevision';
+`;
+
+export const myAsyncConditionalRevision2 = patchSQL({
+    when: () => {
+        return new Promise( resolve => {
+            setTimeout( ()=>{
+                resolve( true )
+            }, 15000 );
+        });
+    }
+}).sql`
+select 'myAsyncConditionalRevision';
+`;
