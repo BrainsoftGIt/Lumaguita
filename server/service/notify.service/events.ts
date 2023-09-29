58c9a2de3f6c0aaa77c62cd2353518d70d9d9b74
Daniel Costa <danielcarvalho555@gmail.com>
2023-09-29 14:57:40 +0000

    v2.1.1

--- server/service/notify.service/events.ts
+++ server/service/notify.service/events.ts
@@ -3,6 +3,8 @@ import {RevisionRegistry} from "kitres";
 export interface LumaguitaEvents {
     localDataChange( changeCode:string, ... args:any[] ),
     revision(blocks:RevisionRegistry[])
+    userLogged( user:{ user_id:string, user_name:string, user_workspace:string, branch?:string})
+    setsProduct( newProduct:any, oldProduct:any )
     startService(),
     stopService()
 }
