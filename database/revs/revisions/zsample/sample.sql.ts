b2acf1d4bda3e0eae414a73be3aedc8b00134aa2
Daniel Costa <danielcarvalho555@gmail.com>
2023-09-29 09:53:31 +0000

    v2.1.1

--- database/revs/revisions/zsample/sample.sql.ts
+++ database/revs/revisions/zsample/sample.sql.ts
@@ -26,52 +26,58 @@ export const myConditionalRevision3 = patchSQL({
         return myCondition;
     }
 }).sql`
-select 'myConditionalRevision3';
+select 'myConditionalRevision  snss3';
 `;
 
-export const myAsyncConditionalRevision3 = patchSQL({
-    when: () => {
-        return new Promise( resolve => {
-            setTimeout( ()=>{
-                resolve( true )
-            }, 5000 );
-        });
-    }
+export const myUniqueSQL = patchSQL({
+    unique: true
 }).sql`
-select 'myAsyncConditionalRevision';
-`;
-export const myAsyncConditionalRevision4 = patchSQL({
-    when: () => {
-        return new Promise( resolve => {
-            setTimeout( ()=>{
-                resolve( true )
-            }, 10000 );
-        });
-    }
-}).sql`
-select 'myAsyncConditionalRevision';
+select 'myUniqueSQL 44456556';
 `;
 
-export const myAsyncConditionalRevision = patchSQL({
-    when: () => {
-        return new Promise( resolve => {
-            setTimeout( ()=>{
-                resolve( true )
-            }, 20000 );
-        });
-    }
-}).sql`
-select 'myAsyncConditionalRevision';
-`;
-
-export const myAsyncConditionalRevision2 = patchSQL({
-    when: () => {
-        return new Promise( resolve => {
-            setTimeout( ()=>{
-                resolve( true )
-            }, 15000 );
-        });
-    }
-}).sql`
-select 'myAsyncConditionalRevision';
-`;
+// export const myAsyncConditionalRevision3 = patchSQL({
+//     when: () => {
+//         return new Promise( resolve => {
+//             setTimeout( ()=>{
+//                 resolve( true )
+//             }, 5000 );
+//         });
+//     }
+// }).sql`
+// select 'myAsyncConditionalRevision';
+// `;
+// export const myAsyncConditionalRevision4 = patchSQL({
+//     when: () => {
+//         return new Promise( resolve => {
+//             setTimeout( ()=>{
+//                 resolve( true )
+//             }, 10000 );
+//         });
+//     }
+// }).sql`
+// select 'myAsyncConditionalRevision';
+// `;
+//
+// export const myAsyncConditionalRevision = patchSQL({
+//     when: () => {
+//         return new Promise( resolve => {
+//             setTimeout( ()=>{
+//                 resolve( true )
+//             }, 20000 );
+//         });
+//     }
+// }).sql`
+// select 'myAsyncConditionalRevision';
+// `;
+//
+// export const myAsyncConditionalRevision2 = patchSQL({
+//     when: () => {
+//         return new Promise( resolve => {
+//             setTimeout( ()=>{
+//                 resolve( true )
+//             }, 15000 );
+//         });
+//     }
+// }).sql`
+// select 'myAsyncConditionalRevision';
+// `;
--- server/service/database.service/kitres/revison.ts
+++ server/service/database.service/kitres/revison.ts
@@ -16,16 +16,18 @@ pgRevision.on("collectError", error =>{
 });
 
 pgRevision.on( "register", block => {
-    let filename = scriptUtil.typescriptOf( block.filename );
+    // console.log( block )
+    let filename = scriptUtil.typescriptOf( block.filename ) || block.filename;
     let lineNumber = block.line?.line as any;
     if( lineNumber ) lineNumber = `:${ lineNumber }`;
     console.log( `MAGUITA> collecting database path ${ new URL(`file:\\\\${ filename }${lineNumber}`).href } identifier = "${ block.identifier }"` );
 });
 
 pgRevision.on( "applierNotice", notice => {
-    let filename = scriptUtil.typescriptOf( notice.filename );
+    let filename = scriptUtil.typescriptOf( notice.filename )||notice.filename;
     let lineNumber = notice.line?.line as any;
     if( lineNumber ) lineNumber = `:${ lineNumber }`;
+    else lineNumber = ':1';
     let status = chalk.blueBright.bold( notice.status );
     if( notice.status === "ERROR" ) status = chalk.redBright.bold( notice.status );
     if( notice.status === "FINALIZED" ) status = chalk.green.bold( notice.status );
