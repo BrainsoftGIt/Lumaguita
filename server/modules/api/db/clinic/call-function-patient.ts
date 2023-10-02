02169edba628ee1ddec458b7bb8d846a47853c21
ahmedjorge <ahmedferreira11@gmail.com>
2023-10-02 19:59:51 +0000

    commit laucher

--- server/modules/api/db/clinic/call-function-patient.ts
+++ server/modules/api/db/clinic/call-function-patient.ts
@@ -1,7 +1,7 @@
 import { catchAll, catchLast, Templates } from "zoo.pg";
 
 export function functSetPatient(args) {
-    const factoryClinic = require("../../../../service/database.service/clinica.factory");
+    const {factoryClinic} = require("../../../../service/database.service/clinica.factory");
     const {sql} = factoryClinic.create(Templates.PARAMETERIZED);
     return catchLast(
         sql `select * from clinic.sets_patient( ${ args }) data`
@@ -9,7 +9,7 @@ export function functSetPatient(args) {
 }
 
 export function functLoadPatient(args) {
-    const factoryClinic = require("../../../../service/database.service/clinica.factory");
+    const {factoryClinic} = require("../../../../service/database.service/clinica.factory");
     const {sql} = factoryClinic.create(Templates.PARAMETERIZED);
     return catchAll(
         sql `select * from clinic.funct_load_patient( ${ args }) data`
