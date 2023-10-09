9910380dff09954d58a1ed54ca12196c461f83d3
ahmedjorge <ahmedferreira11@gmail.com>
2023-10-09 18:39:54 +0000

    commit laucher

--- client/public/assets/js/admin/reports.js
+++ client/public/assets/js/admin/reports.js
@@ -404,14 +404,18 @@ var report = {
             return
         }
 
+
+        let {value: year} = $("#financa_report_yaer li.active").data();
+        let {value: month} = $("#financa_report_month li.active").data();
+
         $("body").addClass("loading");
         $.ajax({
             url: "/api/report/export/imposto",
             method: "POST",
             contentType: "application/json",
             data: JSON.stringify({
-                arg_datainicio: $("#financa_report_start").val().stringToDate().getDateEn(),
-                arg_datafim: $("#financa_report_end").val().stringToDate().getDateEn(),
+                arg_datainicio: new Date(year, month-1, 1).getDateEn(),
+                arg_datafim: new Date(year, month, 0).getDateEn()
             }),
             success(ordenList) {
                 $("body").removeClass("loading");
@@ -724,3 +728,10 @@ $("#footer_values_type").on("click", "li", function () {
 });
 
 $('[data-inputmask-alias]').inputmask();
+xTableGenerate();
+
+var financa_report_yaer = 2022;
+while (financa_report_yaer <= new Date().getFullYear()){
+    $("#financa_report_yaer").append(`<li class="tgl" data-value="${financa_report_yaer}">${financa_report_yaer}</li>`);
+    financa_report_yaer++;
+}
