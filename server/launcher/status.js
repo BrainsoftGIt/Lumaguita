c1cb1c7ea6825fe42aaf2cf87e33c976e7c80953
Daniel Costa <danielcarvalho555@gmail.com>
2023-10-10 20:08:33 +0000

    v2.1.1

--- server/launcher/status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requireLauncher = exports.launcherStatus = void 0;
-exports.launcherStatus = {};
-function requireLauncher() {
-    return require(exports.launcherStatus.launcherName);
-}
-exports.requireLauncher = requireLauncher;
-//# sourceMappingURL=status.js.map
--- server/service/web.service/middlewares/cookie.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../index");
-const cookieParser = require('cookie-parser');
-index_1.app.use(cookieParser());
-//# sourceMappingURL=cookie.js.map
