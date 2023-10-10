baed81f579e4a6bb535fe18231e1f70d70d04c62
Daniel Costa <danielcarvalho555@gmail.com>
2023-10-10 20:07:27 +0000

    v2.1.1

--- build/kconst/events/default.event.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../index");
-index_1.kconst.on((key, value, oldValue, replace) => {
-    //TODO listener const change here
-});
-//# sourceMappingURL=default.event.js.map
