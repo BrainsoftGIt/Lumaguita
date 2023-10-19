9d7c4848063bc328cb51c35d3c62e9f7cec56bdd
ahmedjorge <ahmedferreira11@gmail.com>
2023-10-19 18:37:20 +0000

    commit laucher

--- client/public/assets/js/serieOperation.js
+++ client/public/assets/js/serieOperation.js
@@ -1,8 +1,13 @@
 var serieOperation = {
-    tipo:{
-       fatura: 1,
-       fatura_recibo: 2,
-       recibo: 3
+    tipo: {
+        fatura: 1,
+        fatura_recibo: 2,
+        recibo: 3,
+        notaCredito: 4,
+        guiaSaida: 5,
+        faturaProforma: 6,
+        faturaSimplificada: 7,
+        notaDebito: 8,
     },
     missing: [],
     loadSerieOperation(series, place="admin"){
@@ -12,7 +17,7 @@ var serieOperation = {
             contentType: "application/json",
             data: JSON.stringify({place: place, list_type_series: series}),
             success(e) {
-                serieOperation.missing = e.series;
+                serieOperation.missing = serieOperation.missing.concat(e.series);
             }
         });
     },
