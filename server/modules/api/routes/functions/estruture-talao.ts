acb663d44bcb32592bcdaa5eb855ebaf5fc1f774
ahmedjorge <ahmedferreira11@gmail.com>
2023-10-25 10:56:06 +0000

    commit laucher

--- server/modules/api/routes/functions/estruture-talao.ts
+++ server/modules/api/routes/functions/estruture-talao.ts
@@ -1,5 +1,6 @@
 export let structure = ({margin}) => {
-    let { marginLeft: left, marginRight : right } = margin || {}
+    let { marginLeft: left, marginRight : right } = margin || {};
+    console.log(({left, right}))
     return {
         styles: {
             pequena: {
@@ -22,7 +23,7 @@ export let structure = ({margin}) => {
                 bold: true,
             },
         },
-        pageMargins: [ left || 5, 0, right || 15, 0 ],
+        pageMargins: [ (+left || 5), 0, (+right || 15), 0 ],
         pageSize: {
             width : 210,
             height: 595 * 2
