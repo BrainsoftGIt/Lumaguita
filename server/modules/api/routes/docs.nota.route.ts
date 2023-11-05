d2e9e8e28921579313d9aa06ed5627498239d631
ahmedjorge <ahmedferreira11@gmail.com>
2023-11-05 22:29:43 +0000

    commit laucher

--- server/modules/api/routes/docs.nota.route.ts
+++ server/modules/api/routes/docs.nota.route.ts
@@ -27,6 +27,30 @@ app.post( "/api/load/fatura/to/credito/nota", (req, res, next) => {
     })
 });
 
+app.post( "/api/load/doc/to/nota", (req, res, next) => {
+    let _session = getUserSession( req );
+
+    let args = req.body;
+    args.arg_colaborador_id = _session.user_id;
+    args.arg_espaco_auth = _session.workspace;
+
+    dbRes.call.tweeks.funct_load_conta_docs_financa({ args }, (error, result) => {
+        if( error ){
+            res.json({
+                result:false,
+                message: error.message,
+                hint: error
+            })
+            console.error( error );
+            return;
+        }
+
+        return res.json({
+            fatura: result.rows?.[0] || {},
+        })
+    })
+});
+
 app.post( "/api/reg/credito/nota", (req, res, next) => {
     let _session = getUserSession( req );
 
