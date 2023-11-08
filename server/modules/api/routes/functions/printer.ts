d978a579253f63319f10ba813168450b1da8c450
Ahmed Jorge Ferreira <ahmed.ferreira.ext@unitel.st>
2023-11-08 12:41:49 +0000

    commit print alteration

--- server/modules/api/routes/functions/printer.ts
+++ server/modules/api/routes/functions/printer.ts
@@ -19,7 +19,7 @@ import { exec } from 'child_process';
 export let print = (printerName: string, filePDF: string, paper = "POS") => {
 
 // Replace the command with the appropriate command to print a PDF
-    let command = `AcroRd32.exe" /t "${filePDF}" "${printerName}"`;
+    let command = `SumatraPDF.exe" -print-to "${filePDF}" "${printerName}"`;
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
