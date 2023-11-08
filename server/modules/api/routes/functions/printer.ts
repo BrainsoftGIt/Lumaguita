f3c26a26a28e96e4e9d42e4bd197833bdb5f8ffe
Ahmed Jorge Ferreira <ahmed.ferreira.ext@unitel.st>
2023-11-08 18:51:43 +0000

    commit print alteration

--- server/modules/api/routes/functions/printer.ts
+++ server/modules/api/routes/functions/printer.ts
@@ -1,28 +1,12 @@
-/*import {JavaCaller} from "java-caller";
-import path from "path";
-
-export let print = (printerName, filePDF, paper = "POS") => {
-
-    //language=file-reference
-    const java = new JavaCaller({
-        jar: path.relative(process.cwd(), path.join(__dirname, "../../../../../libs/PrinterPDF.jar"))
-    });
-
-    java.run([ "-printer", printerName, "-file", filePDF, "-paper", paper]).then( ({status, stdout, stderr}) => {
-        console.log( stdout, stderr, status )
-    });
-}*/
-
 import { exec } from 'child_process';
 export let print = (printerName: string, filePDF: string, paper = "POS") => {
 
 // Replace the command with the appropriate command to print a PDF
-    let command = `SumatraPDF.exe -print-to "${printerName}\" \"${filePDF}\"`;
-    console.log({command})
+    let command = `SumatraPDF.exe -print-to "${printerName}" "${filePDF}"`;
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error: ${error.message}`);
+            console.error(`Error: ${error.message}`, "stderr", stderr);
             return;
         }
         if (stderr) {
