fec0074e657cb215c434ab8860fbd9c23ebdcda0
Daniel Costa <danielcarvalho555@gmail.com>
2023-11-24 08:54:33 +0000

    v2.1.1

--- database/revs/revisions/critical/2023-11/rev-10005-posto-multiple-serie.sql.ts
+++ database/revs/revisions/critical/2023-11/rev-10005-posto-multiple-serie.sql.ts
@@ -179,3 +179,91 @@ $$
     end;
     $$;
 `;
+
+
+export const funct_pos_load_posto = sql`
+create or replace function tweeks.funct_pos_load_posto(args jsonb) returns SETOF jsonb
+  language plpgsql
+as
+$$
+declare
+  /**
+    Essa função serve para devolver o cambio do dia de uma moeda
+    filter := {
+      arg_chave_temporaria: CHAVE;
+    }
+  */
+
+  _const map.constant;
+  _chave_doc jsonb;
+  _posto tweeks.posto;
+  _posto_disponivel boolean;
+  _local cluster.cluster;
+begin
+  _const := map.constant();
+  _local := cluster._get_cluster_local();
+  select * into _chave_doc
+    from tweeks.funct_load_chave( args );
+
+  _posto := jsonb_populate_record( _posto, _chave_doc );
+  _posto_disponivel := _chave_doc->>'posto_disponivel';
+
+  if _chave_doc is not null and not _posto_disponivel or _posto.posto_id is null then
+    return next _chave_doc;
+    return;
+  elseif _chave_doc is null then
+    return;
+  end if;
+
+  return next _chave_doc;
+
+  return query
+    with __cambio as (
+      select
+        c.currency_id,
+        c.currency_name,
+        c.currency_code,
+        cb.cambio_id,
+        cb.cambio_taxa,
+        cb.cambio_data,
+        cb.cambio_estado,
+        cb.cambio_dataatualizacao,
+        cb.cambio_espaco_auth,
+        rank( ) over ( partition by cb.cambio_currency_id, cb.cambio_espaco_auth order by cb.cambio_dataregistro desc ) as cambio_rank
+      from tweeks.cambio cb
+        inner join geoinfo.currency c on cb.cambio_currency_id = c.currency_id
+      where cb.cambio_data <= current_timestamp::date
+        and cb.cambio_estado != _const.maguita_cambio_estado_anulado
+      order by cb.cambio_dataregistro desc
+    ), __space_cambio as (
+      select
+          e.espaco_id,
+          e.espaco_nome,
+          e.espaco_nivel,
+          e.espaco_vender,
+          al.aloca_id,
+          al.aloca_serie_faturarecibo,
+          al.aloca_serie_fatura,
+          al.aloca_estado,
+          al.aloca_montante,
+          _local.cluster_identifier,
+          _local.cluster_name,
+          _local.cluster_domain,
+          _local.cluster_port,
+          array_agg( c ) as espaco_cambios
+        from __cambio c
+          inner join tweeks.espaco e on c.cambio_espaco_auth = e.espaco_id
+            and e.espaco_estado = _const.maguita_espaco_estado_ativo
+          inner join tweeks.aloca al on e.espaco_id = al.aloca_espaco_destino
+            and al.aloca_posto_id = _posto.posto_id
+            and al.aloca_estado = _const.maguita_aloca_estado_ativo
+        where c.cambio_rank = 1
+          and e.espaco_vender = _local.cluster_identifier
+        group by al.aloca_id,
+          e.espaco_id
+    ) select to_jsonb( sc )
+      from __space_cambio sc
+  ;
+end;
+$$;
+`;
