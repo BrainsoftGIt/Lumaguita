73a07c450067aad5cd8604a632426c5c2ad7b39d
Daniel Costa <danielcarvalho555@gmail.com>
2023-11-25 08:35:36 +0000

    v2.1.1

--- server/global/pid.ts
+++ server/global/pid.ts
@@ -56,31 +56,31 @@ export function captureKill() {
 export function showUncaughtError(){
     if( args.appMode === "dev" ) return;
 
-    // process.on('unhandledRejection', (reason, p) => {
-    //     console.log( "[[================ [Unhandled Rejection at Promise] ================" );
-    //     console.error( reason?.toString?.() );
-    //     p.catch( reason1 => {
-    //         console.error( reason1?.toString?.() );
-    //     })
-    //     console.log( "================ Unhandled Rejection at Promise ================]]" );
-    // });
-    // process.on('uncaughtException', err => {
-    //     console.log( "[[================ [Uncaught Exception Thrown] ================" );
-    //     console.error( err?.toString?.() );
-    //     console.log( "================ Uncaught Exception thrown ================]]" );
-    // });
-    //
-    // process.on('uncaughtExceptionMonitor', err => {
-    //     console.log( "[[================ [Uncaught Exception Exception Monitor] ================" );
-    //     console.error( err?.toString?.() );
-    //     console.log( "================ Uncaught Exception Exception Monitor ================]]" );
-    // });
-    //
-    // process.on('rejectionHandled', err => {
-    //     console.log( "[[================ [Rejection Handled] ================" );
-    //     console.error( err?.toString?.() );
-    //     console.log( "================ Rejection Handled ================]]" );
-    // });
+    process.on('unhandledRejection', (reason, p) => {
+        console.log( "[[================ [Unhandled Rejection at Promise] ================" );
+        console.error( reason?.toString?.() );
+        p.catch( reason1 => {
+            console.error( reason1?.toString?.() );
+        })
+        console.log( "================ Unhandled Rejection at Promise ================]]" );
+    });
+    process.on('uncaughtException', err => {
+        console.log( "[[================ [Uncaught Exception Thrown] ================" );
+        console.error( err?.toString?.() );
+        console.log( "================ Uncaught Exception thrown ================]]" );
+    });
+
+    process.on('uncaughtExceptionMonitor', err => {
+        console.log( "[[================ [Uncaught Exception Exception Monitor] ================" );
+        console.error( err?.toString?.() );
+        console.log( "================ Uncaught Exception Exception Monitor ================]]" );
+    });
+
+    process.on('rejectionHandled', err => {
+        console.log( "[[================ [Rejection Handled] ================" );
+        console.error( err?.toString?.() );
+        console.log( "================ Rejection Handled ================]]" );
+    });
 }
 
 export type ExitSignalCallback = ( exitCode:number )=> void;
