66e4aefc408f54d6445996eee811de04ff635479
ahmedjorge <ahmedferreira11@gmail.com>
2023-12-10 18:23:23 +0000

    commmit melhoria fatura to pos

--- server/modules/api/routes/efatura.route.ts
+++ server/modules/api/routes/efatura.route.ts
@@ -212,10 +212,6 @@ app.get("/api/efatura/report/excel/:data", async (req, res) => {
             codigo_isento = +codigo_isento;
         }
 
-        if(codigo_isento){
-            codigo_isento = +codigo_isento;
-        }
-
         documento_serie = `${tserie_code || "N/A"}${documento_serie ?? ""}`
         workSheet.addRow([documento_numero, documento_serie, documento_data, nif_consumidor, total_valor_itens, taxa_aplicavel_itens, codigo_isento, quant_itens, desc_itens, numero_documento_origem, data_documento_origem, tipo_documento_origem]);
 
