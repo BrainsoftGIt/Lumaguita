00454c25e46ab383dd2cf58dc32337dac021a7a0
ahmedjorge <ahmedferreira11@gmail.com>
2023-12-11 16:14:41 +0000

    commmit melhoria fatura to pos

--- server/modules/api/routes/docs.nota.route.ts
+++ server/modules/api/routes/docs.nota.route.ts
@@ -1,7 +1,9 @@
 import {app, storage} from '../../../service/storage.service';
-import {getUserSession} from "./functions/get-session";
+import {getUserSession, getUserSessionPOS} from "./functions/get-session";
 import {dbRes} from "../../../service/database.service/kitres/res";
 import {Result} from "kitres";
+import {functLoadContaData} from "../db/call-function-pos";
+import {functLoadDepositoData} from "../db/call-function-contacorrrente";
 app.post( "/api/load/doc/to/nota", (req, res, next) => {
     let _session = getUserSession( req );
 
@@ -79,4 +81,32 @@ app.post( "/api/load/documents", (req, res, next) => {
             return res.json(result?.rows)
         }
     }).doc()
+});
+
+app.post( "/api/get/number/document", (req, res, next) => {
+    let dados = req.body;
+    let _session = (!!dados.admin) ? getUserSession( req ) : getUserSessionPOS( req );
+
+    if(dados.conta_id) {
+        functLoadContaData({
+            arg_conta_id: dados.conta_id,
+            with_client: true,
+            arg_espaco_auth: _session.workspace,
+            arg_colaborador_id: _session.user_id
+        }).then(({rows}) => {
+            let {0: {main: {conta_serie: {document}}}} = rows;
+            res.json({
+                document
+            })
+        });
+    }
+
+    if(dados.deposito) {
+        functLoadDepositoData({deposito_id: dados.deposito}).then(({rows}) => {
+            let {0: {data: {deposito_documento: document}}} = rows;
+            res.json({
+                document
+            })
+        })
+    }
 });
