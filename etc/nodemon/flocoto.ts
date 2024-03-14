092a72d912b8415501e110d12a94663762f72404
Daniel Costa <danielcarvalho555@gmail.com>
2024-03-14 20:47:09 +0000

    v2.1.1

--- etc/nodemon/flocoto.ts
+++ etc/nodemon/flocoto.ts
@@ -1,24 +1,16 @@
-import {fork} from "child_process";
-import Path from "path";
 import {hideBin} from "yargs/helpers";
 import yargs from "yargs";
-import {manifest} from "../../server/context";
+import { flocoto } from "kitres";
+import Path from "path";
 
 const argv = yargs( hideBin(process.argv))
     .option("isolated", {
         type: "boolean",
     }).parse();
 
-
-
-let flocotoRoot = Path.join( __dirname, /*language=file-reference*/ "../../../flocoto" );
-let flocotoMainFile = Path.join( flocotoRoot, "server/launcher/index.js" );
-const isolated = argv["isolated"];
-
-let args = [];
-if( isolated ) args.push( "--flocoto", manifest.name );
-fork( flocotoMainFile, args, {
-    cwd: flocotoRoot
+let isolated = argv["isolated"];
+flocoto.launcher( Path.join( __dirname, /*language=file-reference*/ "../../"), {
+    isolated: isolated
 });
 
 
