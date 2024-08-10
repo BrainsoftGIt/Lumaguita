cb0ae16ea4544c31e3cd46c189bdd98e635d7012
ahmedjorge <ahmedferreira11@gmail.com>
2024-08-10 16:12:47 +0000

    commit atual resolução!

--- client/public/assets/js/pos/payment.js
+++ client/public/assets/js/pos/payment.js
@@ -598,7 +598,7 @@ $("body").on("keyup", "#montante_entregue",function () {
         let valorPagamento = $("#valorTotalPagamento").text().unFormatter();
         console.log({per: desconto / valorPagamento, desconto, valorPagamento
     })
-        $("#montante_desconto_per").val((desconto / valorPagamento * 100.0).toFixed())
+        $("#montante_desconto_per").val((desconto / valorPagamento * Number.parseFloat("100")).toFixed())
         payment.calcularTroco();
         return
     }
@@ -612,7 +612,7 @@ $("body").on("keyup", "#montante_entregue",function () {
     }
 
     let valorPagamento = $("#valorTotalPagamento").text().unFormatter();
-    $("#montante_desconto").val((valorPagamento * per / 100.0).formatter())
+    $("#montante_desconto").val((valorPagamento * per / Number.parseFloat("100")).formatter())
 
     payment.calcularTroco();
 });
