fbbc4537f04bb4d238eda9927a3200cbc1a305ef
zootakuxy <danielcarvalho555@gmail.com>
2025-04-14 04:33:26 +0000

    search patch

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd $(dirname "$0")/..
+POSTGRES_STATUS_D="/status.d/${POSTGRES_SHARE_D}"
 
 # Aguardar até o arquivo de estado ser criado
-until [ -f /postgres-status.d/READY ]; do
+until [ -f "${POSTGRES_STATUS_D}/READY" ]; do
   echo "Banco de dados ainda não está pronto. Aguardando..."
   sleep 2
 done
@@ -11,8 +12,6 @@ until pg_isready -h "${POSTGRES_HOST:-postgres}" -p "${POSTGRES_PORT:-5432}" -U 
   echo "Waiting for luma to be ready..."
   sleep 2
 done
-
-
 
 until pg_isready -h "${CLINICA_POSTGRES_HOST:-postgres}" -p "${CLINICA_POSTGRES_PORT:-5432}" -U "${CLINICA_DATABASE_USERNAME:-clinic}" -d "${CLINICA_DATABASE_NAME:-clinic}"; do
   echo "Waiting for clinic to be ready..."
